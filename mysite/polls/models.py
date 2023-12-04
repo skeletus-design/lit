b826402a37080f27e08f497c19809e30a3cdf6ba
skeletus-design <nikita.voro24nin@gmail.com>
2023-12-04 21:40:32 +0300

    Модели книг и пользователей

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    pdf_file = models.FileField(upload_to='pdfs/')
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.CharField(max_length=50)
+    comment = models.TextField()
